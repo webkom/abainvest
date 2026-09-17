6ebb550dba566d883940133c0777eca647e1ed33
celiacisternas <118000765+celiacisternas@users.noreply.github.com>
2026-09-17 18:35:48 +0200

    added folder structure for components and routing
    * react framework added
    
    * added routing
    
    * added folders
    
    * added links in header
    
    * added ffolders for components
    
    * Commit only code changes
    
    * Remove lockfile from branch

---
+++ src/pages/frontPage/index.tsx
@@ -0,0 +1,15 @@
+import Header from "../../components/Header/Header"
+
+
+export default function FrontPage() {
+  return (
+    <>
+        <Header/>
+        <div>
+            Hei, dette er forsiden!
+        </div>
+    </>
+
+  );
+}
+
