3eb00fb424a36209811706ba0ac25e257acf2886
Celia Schmidt-Cisternas <Celia@schmidt-cisternas.net>
2026-09-17 18:52:05 +0200

    minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import './App.css'
 import FrontPage from "./pages/frontPage/index.tsx";
 import About from './pages/aboutUs/index.tsx';
