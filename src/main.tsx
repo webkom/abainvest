4e92cd9aa92156f000d009ef14814a9fb4bca3bb
Celia Schmidt-Cisternas <Celia@schmidt-cisternas.net>
2026-09-26 00:03:28 +0200

    made header sticky + added global.css

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import "./styles/global.css";
 
 createRoot(document.getElementById('root')!).render(
     <BrowserRouter>
